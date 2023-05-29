b49d28619026d052bc3de0d94bb5514745afe177
redplusblue <samirkabra1161@gmail.com>
2023-05-29 18:55:44 -0400

    Created functions to make changes to DOM based on sampleGame

--- src/helpers/layout.js
+++ src/helpers/layout.js
@@ -1,3 +1,6 @@
+import AI from "../factories/AI";
+import Player from "../factories/Player";
+
 const createLayout = () => {
   const gameBoardContainer = document.createElement("div");
   gameBoardContainer.className = "game-board-container";
@@ -7,7 +10,7 @@ const createLayout = () => {
   playerSide.className = "player-side";
   const playerGameBoard = document.createElement("div");
   playerGameBoard.className = "game-board";
-  for (let i = 0; i < 10; i++) {
+  for (let i = 9; i >= 0; i--) {
     // Create row divs
     const row = document.createElement("div");
     row.className = "row";
@@ -16,7 +19,7 @@ const createLayout = () => {
       // Create column divs
       const col = document.createElement("div");
       col.className = "col";
-      col.id = `${i}${j}`;
+      col.id = `P${i}${j}`;
       row.appendChild(col);
     }
 
@@ -28,7 +31,8 @@ const createLayout = () => {
   computerSide.className = "computer-side";
   const computerGameBoard = document.createElement("div");
   computerGameBoard.className = "game-board";
-  for (let i = 0; i < 10; i++) {
+  computerGameBoard.classList.add("computer-board");
+  for (let i = 9; i >= 0; i--) {
     // Create row divs
     const row = document.createElement("div");
     row.className = "row";
@@ -37,7 +41,7 @@ const createLayout = () => {
       // Create column divs
       const col = document.createElement("div");
       col.className = "col";
-      col.id = `${i}${j}`;
+      col.id = `C${i}${j}`;
       row.appendChild(col);
     }
 
@@ -61,4 +65,54 @@ const createLayout = () => {
   gameBoardContainer.appendChild(computerSide);
 };
 
-export { createLayout };
+// Function to place the ships from the gameboard into the board on the DOM
+const gameboardToBoard = (player) => {
+  let playerBoard = player.gameboard.board;
+
+  // Loop through the player's board and for non-empty cells, add a class of occupied
+  for (let i = 0; i < playerBoard.length; i++) {
+    for (let j = 0; j < playerBoard[i].length; j++) {
+      if (playerBoard[i][j] !== null) {
+        let cell = document.getElementById(`P${i}${j}`);
+        cell.classList.add("occupied");
+      }
+    }
+  }
+};
+
+/**
+ * The updateBoard function updates the board on the
+ * DOM with the player's and the AI's hits and misses
+ *
+ * @param {Player} player The player object
+ * @param {AI} ai The AI object
+ */
+const updateBoard = (player, ai) => {
+  const playerHits = player.gameboard.hits;
+  const playerMisses = player.gameboard.missedShots;
+  const aiHits = ai.gameboard.hits;
+  const aiMisses = ai.gameboard.missedShots;
+
+  for (let i = 0; i < playerHits.length; i++) {
+    let cell = document.getElementById(
+      `P${playerHits[i][0]}${playerHits[i][1]}`
+    );
+    cell.classList.add("hit");
+  }
+  for (let i = 0; i < playerMisses.length; i++) {
+    let cell = document.getElementById(
+      `P${playerMisses[i][0]}${playerMisses[i][1]}`
+    );
+    cell.classList.add("miss");
+  }
+  for (let i = 0; i < aiHits.length; i++) {
+    let cell = document.getElementById(`C${aiHits[i][0]}${aiHits[i][1]}`);
+    cell.classList.add("hit");
+  }
+  for (let i = 0; i < aiMisses.length; i++) {
+    let cell = document.getElementById(`C${aiMisses[i][0]}${aiMisses[i][1]}`);
+    cell.classList.add("miss");
+  }
+};
+
+export { createLayout, gameboardToBoard, updateBoard };
