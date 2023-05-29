746023170c060368d5e7f14b4abcedfbfc1312b3
redplusblue <samirkabra1161@gmail.com>
2023-05-29 18:56:04 -0400

    UI changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./style.css";
-import { createLayout } from "./helpers/layout";
+import { createLayout, gameboardToBoard, updateBoard } from "./helpers/layout";
+import Player from "./factories/Player";
+import Gameboard from "./factories/Gameboard";
+import Ship from "./factories/Ship";
+import AI from "./factories/AI";
 
 createLayout();
