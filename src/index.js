360ed5737354104e8ad53821b18120b789b86f78
redplusblue <samirkabra1161@gmail.com>
2023-05-29 20:19:52 -0400

    Cleared index.js, added gameloop import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import { createLayout, gameboardToBoard, updateBoard } from "./helpers/layout";
+import { gameloop } from "./helpers/gameloop";
 import Player from "./factories/Player";
 import Gameboard from "./factories/Gameboard";
 import Ship from "./factories/Ship";
 import AI from "./factories/AI";
 
 createLayout();
+gameloop();
