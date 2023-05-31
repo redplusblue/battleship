8dae9383c878a837713f4a94bd1c5de3e1458bb6
redplusblue <samirkabra1161@gmail.com>
2023-05-30 23:15:17 -0400

    Made gameloop skeleton

--- src/helpers/gameloop.js
+++ src/helpers/gameloop.js
@@ -1,5 +1,7 @@
 import Player from "../factories/Player";
 import AI from "../factories/AI";
+import Game from "../factories/Game";
+import { addEventListeners, setTurn } from "./layout";
 import { shipCreator, randomShipPlacer } from "./shipWizard";
 // Main loop for the game
 // Idea: Start as an alert() based game, then move to a DOM based game
@@ -14,8 +16,29 @@ export const gameloop = () => {
   playerGameboard.createBoard();
   computerGameboard.createBoard();
   // Create ships and randomly place them on the board
-  const ships = shipCreator().playerShips;
-  randomShipPlacer(playerGameboard, ships);
-  // Random placement of ships successful! 
-  console.log(playerGameboard.board);
+  const ships = shipCreator();
+  randomShipPlacer(playerGameboard, ships.playerShips);
+  randomShipPlacer(computerGameboard, ships.computerShips);
+
+  // IF we create a game object for the gameloop which keep track of scores and players and turns
+  // class Game: Attributes - player, computer, playerScore, computerScore, currentTurn
+  // Methods - attack, checkWin, switchTurns
+  // Game loop
+  const game = new Game(player, computer);
+  addEventListeners(player, computer);
+  let playerMoves = 0;
+  let computerMoves = 0;
+  while (!game.checkWin()) {
+    if (game.currentTurn === player) {
+      // Somehow make it wait for the player to click on a cell
+      if (player.moves.length > playerMoves.length) {
+        game.switchTurns();
+      }
+    } else {
+      computer.attack(player, computer.nextMove());
+      if (computer.moves.length > computerMoves.length) {
+        game.switchTurns();
+      }
+    }
+  }
 };
