632f43a72797080f6ae95a536185e5d070fbbe39
redplusblue <samirkabra1161@gmail.com>
2023-05-30 23:17:25 -0400

    Minor UI changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./style.css";
-import { createLayout, gameboardToBoard, updateBoard } from "./helpers/layout";
+import {
+  addEventListeners,
+  createLayout,
+  gameboardToBoard,
+  updateBoard,
+} from "./helpers/layout";
 import { gameloop } from "./helpers/gameloop";
 import Player from "./factories/Player";
 import Gameboard from "./factories/Gameboard";
@@ -7,4 +12,4 @@ import Ship from "./factories/Ship";
 import AI from "./factories/AI";
 
 createLayout();
-gameloop();
+// gameloop();
