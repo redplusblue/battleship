0664e6c8187d3abfdf4dd2304612c23339eae0dd
redplusblue <samirkabra1161@gmail.com>
2023-06-10 13:13:22 -0400

    Finished gameloop, fixed broken if statement, imported setWinner from layout

--- src/helpers/gameloop.js
+++ src/helpers/gameloop.js
@@ -3,13 +3,14 @@ import AI from "../factories/AI";
 import Game from "../factories/Game";
 import { addEventListeners, setTurn, updateBoard } from "./layout";
 import { shipCreator, randomShipPlacer } from "./shipWizard";
+import { setWinner, gameboardToBoard } from "./layout";
+
 // Main loop for the game
-// Idea: Start as an alert() based game, then move to a DOM based game
-// Step 1: Alerts only for attacks, placements are pre-determined for now
 export const gameloop = () => {
   // Create players
   const player = new Player("Player");
   const computer = new AI();
+  computer.opponent = player;
   // Create and initialize gameboards
   const playerGameboard = player.gameboard;
   const computerGameboard = computer.gameboard;
@@ -19,24 +20,24 @@ export const gameloop = () => {
   const ships = shipCreator();
   randomShipPlacer(playerGameboard, ships.playerShips);
   randomShipPlacer(computerGameboard, ships.computerShips);
-  // IF we create a game object for the gameloop which keep track of scores and players and turns
-  // class Game: Attributes - player, computer, playerScore, computerScore, currentTurn
-  // Methods - attack, checkWin, switchTurns
-  // Game loop
+  gameboardToBoard(player);
+  // Create game object
   const game = new Game(player, computer);
   addEventListeners(player, computer, game);
-  while (!game.checkWin()) {
-    if (game.currentTurn === player) {
-      setTurn("player");
-      // Wait for dom event
-      
-    } else {
+};
+
+export const loop = (player, computer, game) => {
+  if (!game.checkWin()) {
+    if (game.currentTurn !== player) {
       computer.attack(player, computer.nextMove());
       game.switchTurns();
+      setTurn("computer");
     }
     // Update scores
     game.playerScore = computer.gameboard.hits.length;
     game.computerScore = player.gameboard.hits.length;
     updateBoard(player, computer);
+  } else {
+    setWinner(game.checkWin());
   }
 };
