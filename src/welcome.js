5d2ee752982f8d4b76feaf7964ed85b185110800
redplusblue <samirkabra1161@gmail.com>
2023-06-11 10:22:43 -0400

    Finished welcome page

--- src/welcome.js
+++ src/welcome.js
@@ -1,1 +1,9 @@
 import "./styles/welcome.css";
+import "./styles/animations.css";
+import Image from "./assets/Illustration.png";
+
+document.querySelector(".preview").children[0].src = Image;
+
+document.querySelector(".play").children[0].addEventListener("click", () => {
+  window.location.href = "./game.html";
+});
