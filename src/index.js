17abcee915ec8bc418ecd154bf44bd2df3d6cfce
redplusblue <samirkabra1161@gmail.com>
2023-06-10 20:55:41 -0400

    Cleaned up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import "./style.css";
-import {
-  addEventListeners,
-  createLayout,
-  gameboardToBoard,
-  updateBoard,
-} from "./helpers/layout";
+import "./styles/game.css";
+import "./styles/animations.css";
+import { createLayout } from "./helpers/layout";
 import { gameloop } from "./helpers/gameloop";
-import Player from "./factories/Player";
-import Gameboard from "./factories/Gameboard";
-import Ship from "./factories/Ship";
-import AI from "./factories/AI";
 
 createLayout();
-gameloop();
+gameloop()
