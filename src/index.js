405a1c82894b7a0afa92dd66b4a50c80a9df6345
redplusblue <samirkabra1161@gmail.com>
2023-06-11 17:55:14 -0400

    Additions of game-welcome page styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles/game.css";
 import "./styles/animations.css";
+import "./styles/welcome-form.css";
 import { createLayout } from "./helpers/layout";
 import { gameloop } from "./helpers/gameloop";
+import createForm from "./helpers/welcomeForm";
 
-createLayout();
-gameloop()
+document.querySelector(".game-board-container").style.display = "none";
+createForm();
+// createLayout();
+// gameloop();
